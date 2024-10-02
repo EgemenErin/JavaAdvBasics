d39cb6ea5460fc8052fef07958e3e32cc03e47f2
Egemen Erin <pixelsushidev@hotmail.com>
2024-10-02 18:14:11 +0200

    Q2

--- src/main/java/org/example/Q2.java
+++ src/main/java/org/example/Q2.java
@@ -3,13 +3,36 @@ package org.example;
 import java.util.Scanner;
 
 public class Q2 {
+
+    // Define the enum at the class level, outside of the main method
+    enum Season {
+        SPRING, SUMMER, AUTUMN, WINTER
+    }
+
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int number = num.nextInt();
-        num.close();
-        for(int i = 1; i <= 10; i++){
-            System.out.println(number + " x " + i + " = " + (number*i));
+        Scanner fav = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 4 to select a season: ");
+        int choice = fav.nextInt();
+
+        Season selectedSeason;
+        switch (choice) {
+            case 1:
+                selectedSeason = Season.SPRING;
+                break;
+            case 2:
+                selectedSeason = Season.SUMMER;
+                break;
+            case 3:
+                selectedSeason = Season.AUTUMN;
+                break;
+            case 4:
+                selectedSeason = Season.WINTER;
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+                return;
         }
+        System.out.println("You like: " + selectedSeason);
+        fav.close();
     }
 }
